6212ec0f6d5ac9be285578d35fdf067ca6002a17
Agha Huseynov <agahuseynov@hotmail.com>
2019-07-25 07:57:18 +0300

    init

---
+++ web-build/precache-manifest.a9e1a4c7ec1846ac051e13aa761491bd.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "13a4d9cd80a377982413",
+    "url": "/static/js/app.f7e4fba3.chunk.js"
+  },
+  {
+    "revision": "dc7dc9b846d73ed82299",
+    "url": "/static/js/runtime~app.efa64587.js"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "c7578911196974808febf223f05a8364",
+    "url": "/static/media/robot-prod.c7578911.png"
+  },
+  {
+    "revision": "54da1e9816c77e30ebc5920e256736f2",
+    "url": "/static/media/robot-dev.54da1e98.png"
+  },
+  {
+    "revision": "761a29cc5b9163dd2a23118c3d85605d",
+    "url": "/static/media/expo-icon.761a29cc.png"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "61d67dda8c6dc40729689119995305ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2af6a254177aea076225bcd0abc20fc7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "0e65c21119a8d95e8241",
+    "url": "/static/js/2.7a9af5a0.chunk.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  }
+];
